49bf7f76a528466abba48d01ae65b48227bf34c1
donbignose <stamerra17@gmail.com>
2024-10-14 19:50:22 +0200

    fix(db): auto run migrations during connection setup

--- tests/common.rs
+++ tests/common.rs
@@ -1,10 +1,6 @@
 use diesel::prelude::*;
-use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+use on_a_roll::db::connection::run_migrations;
 
-fn run_migrations(connection: &mut SqliteConnection) {
-    connection.run_pending_migrations(MIGRATIONS).unwrap();
-}
 pub fn establish_test_connection() -> SqliteConnection {
     let mut connection =
         SqliteConnection::establish(":memory:").expect("Error creating in-memory database");
